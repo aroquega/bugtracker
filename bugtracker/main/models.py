1cbc2258d6a89bdcb67a14c9fe74b85c4b07a380
Hristo Roque <aroquega@gmail.com>
2023-06-04 08:50:07 -0500

    chore: Crea lista y funcionalidad basica de crear

--- bugtracker/main/models.py
+++ bugtracker/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+## test user, only for development right now
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_projects', null=True)
+    collaborators = models.ManyToManyField(User)
+
+class BugIssue(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    assignee = models.ForeignKey(User, on_delete=models.CASCADE)
--- bugtracker/main/views.py
+++ bugtracker/main/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from main.models import Project
 
-# Create your views here.
+def project_list(request):
+    projects = Project.objects.all()
+    return render(request, 'main/project_list.html', {'projects': projects})
+
+def create_project(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        description = request.POST['description']
+        Project.objects.create(title=title,description=description)
+        return redirect('project_list')
+    return render(request, 'main/project_form.html')
+
+def project_detail():
+    pass
+
